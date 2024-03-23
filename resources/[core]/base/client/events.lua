6229aa0c80c80164add765ab99dd1232e5ff1403
nikit0 <thicsouzaa@outlook.com>
2024-03-23 08:39:34 -0300

    Fix god mode command and player health restoration

--- resources/[core]/base/client/events.lua
+++ resources/[core]/base/client/events.lua
@@ -8,3 +8,16 @@ AddEventHandler("Base:Client:TeleportPlayer",function(Pos)
     local Coords = string.format("%.2f,%.2f,%.2f",Pos["x"],Pos["y"],Pos["z"])
     print("Teleported To Position:",Coords)
 end)
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- GAMEEVENTTRIGGERED
+-----------------------------------------------------------------------------------------------------------------------------------------
+AddEventHandler("gameEventTriggered",function(Name,Args)
+	if Name == "CEventNetworkEntityDamage" then
+		local Ped = PlayerPedId()
+		if GetEntityHealth(Ped) <= 0 then
+            SetEntityHealth(Ped,200)
+
+            print("Player Health Restored")
+		end
+	end
+end)
