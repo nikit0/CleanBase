668f7e224c09854df011f968359dc07bc1e70012
nikit0 <thicsouzaa@outlook.com>
2024-03-23 04:06:32 -0300

    Add new command and update welcome message

--- resources/[core]/base/client/threads.lua
+++ resources/[core]/base/client/threads.lua
@@ -4,9 +4,9 @@
 CreateThread(function()
     while true do
         Wait(0)
-        DwText("Bem-vindo ao ~b~Clean Base",0.032,0.77,0.5)
+        DwText("Welcome to the ~b~Clean Base",0.032,0.77,0.5)
         DwText("~g~Online: ~w~"..GetNumberOfPlayers(),0.16,0.80,0.35)
         DwText("~r~Player: ~w~"..GetPlayerName(PlayerId()),0.16,0.82,0.35)
-        DwText("~y~Comandos F8: ~w~nc | god | tpcds | cds",0.16,0.84,0.35)
+        DwText("~y~Commands F8: ~w~nc | god | tpcds | cds",0.16,0.84,0.35)
     end
 end)
