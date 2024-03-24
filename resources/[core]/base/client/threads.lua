7319ba3d35a2408010e6ec934a63e89f49dd6821
nikit0 <thicsouzaa@outlook.com>
2024-03-24 08:52:34 -0300

    Refactored thread display to use a table of texts

--- resources/[core]/base/client/threads.lua
+++ resources/[core]/base/client/threads.lua
@@ -1,20 +1,26 @@
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- THREAD
 -----------------------------------------------------------------------------------------------------------------------------------------
+local texts = {
+    { "Welcome to ~b~Clean Base",0.040,0.77,0.5 },
+    { "~g~Online: ~w~" .. GetNumberOfPlayers(),0.16,0.80,0.35 },
+    { "~r~Player: ~w~" .. GetPlayerName(PlayerId()),0.16,0.82,0.35 },
+    { "~y~Commands:",0.16,0.84,0.35 },
+    { "nc",0.16,0.86,0.35 },
+    { "god",0.16,0.88,0.35 },
+    { "hud",0.16,0.90,0.35 },
+    { "rec",0.16,0.92,0.35 },
+    { "cds",0.16,0.94,0.35 },
+    { "tpcds ~g~[x,y,z]",0.16,0.96,0.35 }
+}
+
 CreateThread(function()
     while true do
         Wait(1)
         if DisplayHud then
-            DwText("Welcome to ~b~Clean Base",0.040,0.77,0.5)
-            DwText("~g~Online: ~w~"..GetNumberOfPlayers(),0.16,0.80,0.35)
-            DwText("~r~Player: ~w~"..GetPlayerName(PlayerId()),0.16,0.82,0.35)
-            DwText("~y~Commands:",0.16,0.84,0.35)
-            DwText("nc",0.16,0.86,0.35)
-            DwText("god",0.16,0.88,0.35)
-            DwText("hud",0.16,0.90,0.35)
-            DwText("rec",0.16,0.92,0.35)
-            DwText("cds",0.16,0.94,0.35)
-            DwText("tpcds ~g~[x,y,z]",0.16,0.96,0.35)
+            for _,v in pairs(texts) do
+                DwText(v[1],v[2],v[3],v[4])
+            end
         end
 
         DisplayRadar(DisplayHud)
